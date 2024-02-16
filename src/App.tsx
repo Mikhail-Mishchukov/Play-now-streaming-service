d2cf937721e0f1c28ce5ac40e129375cc2a465b0
Mikhail-Mishchukov <mi.mishchukov@gmail.com>
2024-02-17 01:21:56 +0500

    Add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import 'Assets/css/normalize.css';
-import 'Assets/css/fonts.css';
-import 'Assets/css/global.css';
+import 'Assets/styles/normalize.scss';
+import 'Assets/styles/fonts.scss';
+import 'Assets/styles/global.scss';
+
 export function App() {
     return <div>App</div>;
-
 }
